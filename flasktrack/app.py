a1b8e032e9f6a22f51ccfe3d77a18a9c0270079e
stupidcomputer <ryan@beepboop.systems>
2025-01-12 21:20:56 -0600

    make secrets removed from source

--- flasktrack/app.py
+++ flasktrack/app.py
@@ -3,13 +3,13 @@ import json
 
 from flasktrack.appendbase import Appendbase
 
-submit_secret = "test"
 app = Flask(__name__)
+app.config.from_envvar("FLASKTRACK_CREDENTIAL_LOCATION")
 base = Appendbase.from_file("base.json")
 
 @app.route('/<secret>/submit', methods=[ "POST"])
 def handle_new_data(secret):
-    if secret != submit_secret:
+    if secret != app.config["SUBMIT_SECRET"]:
         return ''
     print(request.form)
     base.append(request.form.to_dict())
@@ -19,7 +19,7 @@ def handle_new_data(secret):
 
 @app.route('/<secret>/view')
 def return_monitor_page(secret):
-    if secret != submit_secret:
+    if secret != app.config["SUBMIT_SECRET"]:
         return ''
 
     data_js = "let location_data = " + json.dumps(base.stuff) + ";"
