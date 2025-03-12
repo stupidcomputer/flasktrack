c23b014b11b869b732047dfed402f2b76f61e2a0
stupidcomputer <ryan@beepboop.systems>
2025-03-12 16:55:33 -0500

    allow configuration of database via env var

--- flasktrack/app.py
+++ flasktrack/app.py
@@ -4,16 +4,18 @@ import json
 from flasktrack.appendbase import Appendbase
 
 app = Flask(__name__)
+app.config.from_prefixed_env()
 app.config.from_envvar("FLASKTRACK_CREDENTIAL_LOCATION")
-base = Appendbase.from_file("base.json")
+# configure via FLASK_DATABASE_LOCATION
+base = Appendbase.from_file(app.config["DATABASE_LOCATION"])
 
-@app.route('/<secret>/submit', methods=[ "POST"])
+@app.route('/<secret>/submit', methods=[ "POST" ])
 def handle_new_data(secret):
     if secret != app.config["SUBMIT_SECRET"]:
         return ''
     print(request.form)
     base.append(request.form.to_dict())
-    base.to_file("base.json")
+    base.to_file(app.config["DATABASE_LOCATION"])
 
     return "accepted"
 
